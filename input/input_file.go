546ef9e6f25a1ea9a72364dbd648cc8966fcc1e7
Jitan <jitan.puri@tokopedia.com>
2019-05-19 12:10:54 +0530

    allowed commands check added

--- input/input_file.go
+++ input/input_file.go
@@ -12,6 +12,10 @@ import (
 	"github.com/parking_lot/vehicle"
 )
 
+const (
+	ERR_COMMAND_NOT_ALLOWED = "Command not allowed"
+)
+
 var allowedCommands = map[string]bool{
 	"create_parking_lot": true,
 	"park":               true,
@@ -70,75 +74,80 @@ func ProcessFile(readFile []string) {
 			words := strings.Fields(ilString)
 			if len(words) > 1 {
 				word := words[0]
-				switch word {
-				case "create_parking_lot":
-					slotStr := words[1]
-					slot, err := strconv.ParseInt(slotStr, 10, 64)
-					if err != nil {
-						fmt.Printf("%v\n", err)
-						continue
-					}
 
-					err = parkingLot.InitParkingLot(slot, true)
-					if err != nil {
-						fmt.Println(err)
-					}
+				if _, ok := allowedCommands[word]; ok {
+					switch word {
+					case "create_parking_lot":
+						slotStr := words[1]
+						slot, err := strconv.ParseInt(slotStr, 10, 64)
+						if err != nil {
+							fmt.Printf("%v\n", err)
+							continue
+						}
 
-				case "park":
-					if len(words) > 2 {
+						err = parkingLot.InitParkingLot(slot, true)
+						if err != nil {
+							fmt.Println(err)
+						}
+
+					case "park":
+						if len(words) > 2 {
+							regNo := words[1]
+							color := words[2]
+
+							veh := vehicle.InitVehicle().
+								AddColor(color).
+								AddRegistrationNumber(regNo)
+
+							err := parkingLot.ParkVehicle(*veh)
+							if err != nil {
+								fmt.Printf("%v\n", err)
+							}
+						}
+
+					case "leave":
+						slotStr := words[1]
+						slot, err := strconv.ParseInt(slotStr, 10, 64)
+						if err != nil {
+							fmt.Printf("%v\n", err)
+							continue
+						}
+
+						err = parkingLot.LeaveVehicle(slot)
+						if err != nil {
+							fmt.Printf("%v\n", err)
+						}
+
+					case "status":
+						err := parkingLot.ParkingLotStatus()
+						if err != nil {
+							fmt.Printf("%v\n", err)
+						}
+					case "registration_numbers_for_cars_with_colour":
+						color := words[1]
+
+						_, err := parkingLot.GetRegistrationNumWithColor(color)
+						if err != nil {
+							fmt.Printf("%v\n", err)
+						}
+					case "slot_numbers_for_cars_with_colour":
+						color := words[1]
+
+						_, err := parkingLot.GetSlotNumsForCarWithColor(color)
+						if err != nil {
+							fmt.Printf("%v\n", err)
+						}
+
+					case "slot_number_for_registration_number":
 						regNo := words[1]
-						color := words[2]
 
-						veh := vehicle.InitVehicle().
-							AddColor(color).
-							AddRegistrationNumber(regNo)
-
-						err := parkingLot.ParkVehicle(*veh)
+						_, err := parkingLot.GetSlotWithRegisterationNum(regNo)
 						if err != nil {
 							fmt.Printf("%v\n", err)
 						}
 					}
-
-				case "leave":
-					slotStr := words[1]
-					slot, err := strconv.ParseInt(slotStr, 10, 64)
-					if err != nil {
-						fmt.Printf("%v\n", err)
-						continue
-					}
-
-					err = parkingLot.LeaveVehicle(slot)
-					if err != nil {
-						fmt.Printf("%v\n", err)
-					}
-
-				case "status":
-					err := parkingLot.ParkingLotStatus()
-					if err != nil {
-						fmt.Printf("%v\n", err)
-					}
-				case "registration_numbers_for_cars_with_colour":
-					color := words[1]
-
-					_, err := parkingLot.GetRegistrationNumWithColor(color)
-					if err != nil {
-						fmt.Printf("%v\n", err)
-					}
-				case "slot_numbers_for_cars_with_colour":
-					color := words[1]
-
-					_, err := parkingLot.GetSlotNumsForCarWithColor(color)
-					if err != nil {
-						fmt.Printf("%v\n", err)
-					}
-
-				case "slot_number_for_registration_number":
-					regNo := words[1]
-
-					_, err := parkingLot.GetSlotWithRegisterationNum(regNo)
-					if err != nil {
-						fmt.Printf("%v\n", err)
-					}
+				} else {
+					fmt.Println(ERR_COMMAND_NOT_ALLOWED)
 				}
 			}
 		}
